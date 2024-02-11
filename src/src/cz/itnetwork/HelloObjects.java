762095ba87a27ce1933b22f4b89a0032b798024e
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-11 14:05:28 +0100

    DeleteAhoj

--- src/src/cz/itnetwork/HelloObjects.java
+++ src/src/cz/itnetwork/HelloObjects.java
@@ -1,13 +1,22 @@
 package cz.itnetwork;
 
-public class HelloObjects {
+//public class HelloObjects {
+//
+//    public static void main(String[] args) {
+//        cz.itnetwork.Zdravic zdravic = new cz.itnetwork.Zdravic();
+//        zdravic.text = "Ahoj uživateli";
+//        System.out.println(zdravic.pozdrav("Karel"));
+//        System.out.println(zdravic.pozdrav("Petr"));
+//        zdravic.text = "Vítám tě tu programátore";
+//        System.out.println(zdravic.pozdrav("Richard"));
+//    }
+//}
 
+public class HelloObjects
+{
     public static void main(String[] args) {
-        cz.itnetwork.Zdravic zdravic = new cz.itnetwork.Zdravic();
-        zdravic.text = "Ahoj uživateli";
-        System.out.println(zdravic.pozdrav("Karel"));
-        System.out.println(zdravic.pozdrav("Petr"));
-        zdravic.text = "Vítám tě tu programátore";
-        System.out.println(zdravic.pozdrav("Richard"));
+
+        Zdravic zdravic = new Zdravic();
+        zdravic.pozdrav();
     }
 }
--- src/src/cz/itnetwork/Zdravic.java
+++ src/src/cz/itnetwork/Zdravic.java
@@ -1,19 +1,29 @@
 package cz.itnetwork;
 
+///** Třída reprezentuje zdravič, který slouží ke zdravení uživatelů */
+//class Zdravic
+//{
+//    /** Text pozdravu */
+//    public String text;
+//
+//    /**
+//     * Pozdraví uživatele textem pozdravu a jeho jménem
+//     * @param  jmeno  Jméno uživatele
+//     * @return      Text s pozdravem
+//     */
+//    public String pozdrav(String jmeno)
+//    {
+//        return String.format("%s %s", text, jmeno);
+//    }
+//
+//}
+
 /** Třída reprezentuje zdravič, který slouží ke zdravení uživatelů */
 class Zdravic
 {
-    /** Text pozdravu */
-    public String text;
-
-    /**
-     * Pozdraví uživatele textem pozdravu a jeho jménem
-     * @param  jmeno  Jméno uživatele
-     * @return      Text s pozdravem
-     */
-    public String pozdrav(String jmeno)
+    public void pozdrav()
     {
-        return String.format("%s %s", text, jmeno);
+        System.out.println("Serus");
     }
 
 }

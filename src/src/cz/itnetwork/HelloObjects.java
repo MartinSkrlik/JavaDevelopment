ca6f097d310e4a5fbb9d0c3b4a5ac15800c494c0
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-11 13:30:22 +0100

    DeleteAhoj

---
+++ src/src/cz/itnetwork/HelloObjects.java
@@ -0,0 +1,13 @@
+package cz.itnetwork;
+
+public class HelloObjects {
+
+    public static void main(String[] args) {
+        cz.itnetwork.Zdravic zdravic = new cz.itnetwork.Zdravic();
+        zdravic.text = "Ahoj uživateli";
+        System.out.println(zdravic.pozdrav("Karel"));
+        System.out.println(zdravic.pozdrav("Petr"));
+        zdravic.text = "Vítám tě tu programátore";
+        System.out.println(zdravic.pozdrav("Richard"));
+    }
+}

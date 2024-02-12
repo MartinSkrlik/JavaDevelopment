4c2c8196a081807b3d843ecdc9beb0844517b9b6
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-12 16:55:14 +0100

    NewConstructors, class, methods

---
+++ src/src/tahovyBoj/Bojovnik.java
@@ -0,0 +1,15 @@
+package tahovyBoj;
+
+public class Bojovnik {
+
+    private String jmeno;
+    private int zivot;
+    private int maximalniZivot;
+    private int utok;
+    private int obrana;
+    private String zprava;
+    private Kostka kostka;
+
+
+
+}

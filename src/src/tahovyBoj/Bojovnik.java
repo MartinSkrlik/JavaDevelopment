6e80d5c90eb781060b203c8f67fd6303f2303427
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-13 10:56:51 +0100

    NewConstructors, class, methods

--- src/src/tahovyBoj/Bojovnik.java
+++ src/src/tahovyBoj/Bojovnik.java
@@ -10,6 +10,63 @@ public class Bojovnik {
     private String zprava;
     private Kostka kostka;
 
+    public Bojovnik (String jmeno, int zivot, int utok, int obrana, Kostka kostka) {
+        this.jmeno = jmeno;
+        this.zivot = zivot;
+        this.maximalniZivot = zivot;
+        this.utok = utok;
+        this.obrana = obrana;
+        this.zprava = zprava;
+        this.kostka = kostka;
+    }
 
+    @Override
+    public String toString() {
+        return jmeno;
+    }
+
+//    public boolean jeZivy() {
+//        if (zivot > 1) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+
+    public boolean jeZivy() {
+        return (zivot > 1);
+    }
+
+    public String grafickyZivot () {
+        String grafickyZivot = "[";
+        int celkom = 20;
+        double pocetDilku = Math.round(((double) zivot / maximalniZivot) * celkom);
+        if ((pocetDilku == 0) & (jeZivy())) {
+            pocetDilku = 1;
+        }
+        for (int i = 0; i < pocetDilku; i++) {
+            grafickyZivot += "#";
+        }
+        for (int i = 0; i < celkom - pocetDilku; i++) {
+            grafickyZivot += " ";
+        }
+        grafickyZivot += "]";
+        return grafickyZivot;
+    }
+
+    public void branSe (int uder) {
+        int zraneni = uder - (obrana + kostka.hod());
+        if (zraneni > 0) {
+            zivot -= zraneni;
+            if (zivot <= 0) {
+                zivot = 0;
+            }
+        }
+    }
+
+    public void utoc (Bojovnik souper) {
+        int uder = utok + kostka.hod();
+        souper.branSe(uder);
+    }
 
 }
--- src/src/tahovyBoj/TahovyBoj.java
+++ src/src/tahovyBoj/TahovyBoj.java
@@ -16,5 +16,15 @@ public class TahovyBoj {
         System.out.println("\n\n" + desetistenna);
         for (int i = 0; i < 10; i++)
             System.out.print(desetistenna.hod() + " ");
+
+        Kostka kostka = new Kostka(10);
+        Bojovnik bojovnik = new Bojovnik("Zalgoren", 100, 20, 10, kostka);
+        System.out.printf(" %n%nBojovnik: %s%n", bojovnik);
+        System.out.printf("Nazive: %s%n", bojovnik.jeZivy());
+        System.out.printf("GrafickyZivot: %s%n", bojovnik.grafickyZivot());
+
+        bojovnik.utoc(bojovnik);
+        System.out.printf("Zivot po utoku: %s%n ", bojovnik.grafickyZivot());
+
     }
 }

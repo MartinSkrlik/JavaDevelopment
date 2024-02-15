ce42f1232f0327b3087e67fcd2ab9cbf33ae74e5
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-15 10:53:46 +0100

    NewConstructors, class, methods

---
+++ src/src/Statika/Statika.java
@@ -0,0 +1,14 @@
+package Statika;
+
+public class Statika {
+
+    public static void main(String[] args) {
+//        Uzivatel uzivatel = new Uzivatel("Martin","Hslo");
+//        if (uzivatel.prihlasSe("Hslo")) {
+//            System.out.println("spravne heslo");
+//        } else System.out.println("nespravne heslo");
+
+        System.out.println(Uzivatel.maximalniDelkaHesla);
+    }
+
+}
---
+++ src/src/Statika/Uzivatel.java
@@ -0,0 +1,35 @@
+package Statika;
+
+public class Uzivatel {
+
+    private String jmeno;
+    private String heslo;
+    private boolean prihlaseny;
+    private int id;
+    public static int maximalniDelkaHesla = 6;
+    private static int dalsiId = 1;
+
+    public Uzivatel(String jmeno, String heslo) {
+        this.jmeno = jmeno;
+        this.heslo = heslo;
+        prihlaseny = false;
+        id = dalsiId;
+        dalsiId++;
+    }
+
+    public boolean prihlasSe(String zadaneHeslo) {
+        if (zadaneHeslo.equals(heslo)) { // kontrola hesla
+            prihlaseny = true;
+            return true;
+        }
+        return false;   // hesla nesuhlasia
+    }
+
+//    public static boolean zvalidujHeslo(String heslo) {
+
+    }
+
+
+
+
+}

808b660bc1dce4205b1c5e8f07691a8f369bce10
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-23 16:49:42 +0100

    interface

---
+++ src/src/OOP/Main.java
@@ -0,0 +1,16 @@
+package OOP;
+// Import the LinkedList class
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+        cars.addFirst("sfdf");
+        System.out.println(cars);
+    }
+}

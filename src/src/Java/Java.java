b62f14b90183556df768c844cffea945de25f587
SKRLIK, MARTIN <martin.skrlik@accenture.com>
2024-02-26 16:42:16 +0100

    read file, crate file

--- src/src/Java/Java.java
+++ src/src/Java/Java.java
@@ -1,13 +1,55 @@
 package Java;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Java {
     public static void main (String[] args) {
 
+        // Scanner class
+//        int y,i,o;
+//        Scanner myObj = new Scanner(System.in);
+//        System.out.println("write");
+//        y = myObj.nextInt();
+//        System.out.println(y);
 
+        // Conut words
+//        String sentence = "Napis peknu vetu";
+//        sentence.length();
+//        int veta = sentence.split("\\s").length; // "s\\" count only words
+//        System.out.println(veta);
 
+        // Reverse string
+//        String sentenceSecond = "Hello";
+//        String reversed = "";
+//        for (int i = 0; i < sentenceSecond.length(); i++) {
+//            reversed = sentenceSecond.charAt(i) + reversed;
+//        }
+//        System.out.println(reversed);
 
+        // Calculate the sum of array
+//        int[] sum = {1, 2, 3};
+//        int sum2 = 0;
+//        int sum3;
+//
+//        for (int sum4 : sum) {
+//            sum2 += sum4;
+//        }
+//        System.out.println(sum2);
+
+        // Odd or even number
+//        int odd = 8;
+//
+//        if (odd % 2 == 0) {
+//            System.out.println(odd + " is even");
+//        } else {
+//            System.out.println(odd + " is odd");
+//        }
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate"));
 
 
     }
+    }
 
-}
+
